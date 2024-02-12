3f69b188a8fb883c686b320cb48fb1843b245804
AndresAC14 <andresamo0101@gmail.com>
2024-02-12 22:02:50 +0100

    Avance algoritmos

--- AnalisisExploratorio.py
+++ AnalisisExploratorio.py
@@ -60,80 +60,6 @@ plt.ylabel('Frecuencia')
 plt.show()
 '''
 
-########################################################################################################################
-################################## LO QUE HAY AQUI DENTRO NO ESTA BIEN #################################################
-########################################################################################################################
-
-'''
-# Enfrentar ClaseReal con ClasePred y/o ClasePredPrime (con heatmap)
-# Sacar todos los valores unicos de todas las columnas de clase, ya que pueden repetirse o no y ser totalmente distintos
-# Obtener los valores únicos de cada columna
-valores_ClasePred = df['ClasePred'].unique()
-valores_ClaseReal = df['ClaseReal'].unique()
-valores_ClasePredPrime = df['ClasePredPrime'].unique()
-
-# Combina todos los valores únicos sin repetición
-valores_unicos_totales = set(valores_ClaseReal) | set(valores_ClasePred) | set(valores_ClasePredPrime)
-
-# Puedes imprimir o trabajar con la variable valores_unicos_totales según tus necesidades
-# print(valores_unicos_totales)
-
-# Crear un DataFrame a partir del conjunto de valores únicos
-df_valores_unicos = pd.DataFrame(list(valores_unicos_totales), columns=['Clases'])
-
-# Puedes imprimir o trabajar con el nuevo DataFrame df_valores_unicos según tus necesidades
-# print(df_valores_unicos)
-
-# print(df_valores_unicos.value_counts())
-
-# (df_valores_unicos.shape)
-'''
-
-'''
-# Crear un gráfico de dispersión
-plt.scatter(df['ClaseReal'], df['ClasePred'])
-
-# Agregar etiquetas y título al gráfico
-plt.xlabel('ClaseReal')
-plt.ylabel('ClasePred')
-plt.title('Relación entre ClaseReal y ClasePred')
-
-# Mostrar el gráfico
-plt.show()
-'''
-
-
-'''
-# Crear una matriz de correlación entre las columnas ClaseReal y ClasePred
-correlation_matrix = pd.crosstab(df['ClaseReal'], df['ClasePred'])
-
-# Crear un heatmap
-sns.heatmap(correlation_matrix, annot=True, cmap='inferno', fmt='g')
-plt.xlabel('ClasePred')
-plt.ylabel('ClaseReal')
-plt.title('Heatmap de Relación entre ClaseReal y ClasePred')
-plt.show()
-'''
-
-
-'''
-# Filtrar las filas donde ClaseReal y ClasePred no son iguales
-df_filtered = df[df['ClaseReal'] == df['ClasePred']]
-
-# Crear un gráfico de dispersión con los datos filtrados
-plt.scatter(df_filtered['ClaseReal'], df_filtered['ClasePred'])
-
-# Agregar etiquetas y título al gráfico
-plt.xlabel('ClaseReal')
-plt.ylabel('ClasePred')
-plt.title('Relación entre ClaseReal y ClasePred (Filtrado)')
-
-# Mostrar el gráfico
-plt.show()
-'''
-########################################################################################################################
-########################################################################################################################
-########################################################################################################################
 
 '''
 # Porcentaje de acierto, es decir, (Real == Pred) / Real
@@ -166,7 +92,7 @@ print(df_Porcentaje1)
 '''
 
 print("################################################################################################################")
-'''
+
 # CLASE REAL RESPECTO A CLASE PREDICHA
 
 # print(df_filtered)
@@ -180,7 +106,7 @@ df_2 = df.groupby('ClaseReal')['ClasePred'].count().reset_index(name='Total')
 
 df_Res = pd.merge(df_1, df_2, on='ClaseReal', how='left')
 
-#
+# Crea la columna resultado
 df_Res['Porcentaje'] = ((df_Res['Aciertos'] / df_Res['Total']) * 100).__round__(2)
 
 print(df_Res)
@@ -190,11 +116,90 @@ print(df_Res)
 # plt.show()
 
 # Eso es demasiado grande, asi que mejor hacer la grafica con los que tienen mas porcentaje
-mayores30 = df_Res[df_Res['Porcentaje'] > 70]
-sns.barplot(mayores30, x='ClaseReal', y='Porcentaje')
-plt.show()
-'''
+# mayores = df_Res[df_Res['Porcentaje'] > 70]
+# sns.barplot(mayores, x='ClaseReal', y='Porcentaje')
+# plt.show()
+
 
 # ClasePred vs ClasePredPrime
 # Contar cuantos cambian y que algoritmo estan usando
 
+print("################################################################################################################")
+# Algoritmos que manejamos:  ['Ecualización del histograma'
+#  'Ecualización del histograma adaptativa limitada por contraste'
+#  'Corrección de gamma' 'Transformación logarítmica']
+'''
+# Aciertos para el algoritmo Ecualizacion del histograma
+df_ecualizacion1 = df[(df['ClaseReal'] == df['ClasePred']) & (df['Algoritmo'] == 'Ecualización del histograma')]
+
+df_ecualizacion1 = df_ecualizacion1.groupby('ClaseReal')['ClasePred'].count().reset_index(name='Aciertos')
+
+# Mostrar el DataFrame resultante
+print(df_ecualizacion1.head(5))
+print(df_ecualizacion1['Aciertos'].sum())
+
+
+def aciertosAlgoritmos(df):
+    algoritmos = df['Algoritmo'].unique()
+    df_resultado = pd.DataFrame()
+
+    for alg in algoritmos:
+        # Filtrar las filas donde claseReal es igual a clasePred y Algoritmo es igual al valor actual
+        df_algoritmo_aciertos = df[(df['ClaseReal'] == df['ClasePred']) & (df['Algoritmo'] == alg)]
+
+        # Contar los aciertos para cada clase y reiniciar el índice
+        df_aciertos = df_algoritmo_aciertos.groupby('ClaseReal')['ClasePred'].count().reset_index(name='Aciertos '+alg)
+
+        # Concatenar el DataFrame resultante al DataFrame general
+        df_resultado = pd.concat([df_resultado, df_aciertos], axis=1)
+
+    return df_resultado
+'''
+
+def aciertosAlgoritmos(df, algoritmos):
+    df_resultado = pd.DataFrame()
+
+    for alg in algoritmos:
+        # Filtrar las filas donde claseReal es igual a clasePred y Algoritmo es igual al valor actual
+        df_algoritmo_aciertos = df[(df['ClaseReal'] == df['ClasePred']) & (df['Algoritmo'] == alg)]
+
+        # Contar los aciertos para cada clase y reiniciar el índice
+        df_aciertos = df_algoritmo_aciertos.groupby('ClaseReal').size().reset_index(name='Aciertos '+alg)
+
+        # Concatenar el DataFrame resultante al DataFrame general
+        if df_resultado.empty:
+            df_resultado = df_aciertos
+        else:
+            df_resultado = pd.concat([df_resultado, df_aciertos['Aciertos '+alg]])
+
+    return df_resultado
+
+
+# No se por que al poner el codigo este dentro de la otra funcion no funciona, es raro
+def aciertos(df):
+    algoritmos = df['Algoritmo'].unique()
+    df_aciertos = aciertosAlgoritmos(df, algoritmos)
+    df_aciertos = df_aciertos.replace(np.nan, 0)
+
+    for alg in algoritmos:
+            df_aciertos['Aciertos ' + alg] = df_aciertos['Aciertos ' + alg].astype(int)
+
+    return df_aciertos
+
+
+df_aciertos = aciertos(df)
+print(df_aciertos.head(10))
+print("###############################################################################################################")
+# Ahora quiero sumar los valores de cada columna y ponerlos en una grafica, es decir barplot con los algoritmos
+ # NO FUNCIONA
+def suma(df, aciertos):
+    algoritmos = df['Algoritmo'].unique()
+    suma = pd.DataFrame()
+
+    for alg in algoritmos:
+        suma[alg] = aciertos['Aciertos ' + alg].sum()
+
+    return suma
+
+gr = suma(df, df_aciertos)
+print(gr.head(5))
