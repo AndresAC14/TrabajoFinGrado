6c2cdc9f51728997cf5ea5ff28fe6d599d4b7d3d
Andres Amo Caballero <andres.amo@dazent.es>
2024-04-15 11:47:36 +0200

    ?

--- MineriaDatos.py
+++ MineriaDatos.py
@@ -31,39 +31,51 @@ def flatten_children(item, parent_id=None, parent_name=None):
     return result
 
 
-# Obtén la lista de diccionarios aplanada
+# Aplanar el json
 flattened_data = flatten_children(data)
 
 # Crea un DataFrame a partir de la lista de diccionarios
 df_flat = pd.DataFrame(flattened_data)
-
-# Muestra el DataFrame resultante
 print(df_flat)
 
 # Informacion del dataframe
 # Filas y columnas (2155 filas y 6 columnas)
 print(df_flat.shape)
 
-# Informacion/tipo de las columnas del df
-print(df_flat.info())
-
-# Primeras filas
-# print(df_flat.head(5))
-
 # Columnas -> sift index se pueden quitar
 columnas = df_flat.columns
 print(columnas)
+
+# Informacion/tipo de las columnas del df
+print(df_flat.info())
+
+'''
+Data columns (total 6 columns):
+ #   Column       Non-Null Count  Dtype  
+---  ------       --------------  -----  
+ 0   id           2155 non-null   object 
+ 1   name         2155 non-null   object 
+ 2   sift         1258 non-null   object ---> NO se que es esta columna (aunque creo puede indicar si es hoja o nodo)
+ 3   index        1258 non-null   float64 ---> NO se que es esta columna
+ 4   parent_id    2154 non-null   object 
+ 5   parent_name  2154 non-null   object 
+dtypes: float64(1), object(5)
+'''
+
+# Primeras filas
+# print(df_flat.head(5))
 
 # print(df_flat[df_flat['id'] == 'n03770679'])
 
 # Contar la frecuencia de cada valor en la columna 'id'
 frecuencia_id = df_flat['id'].value_counts()
+print(frecuencia_id.head(10))
 
-# Filtrar las filas donde el valor de 'id' aparezca más de una vez
-filas_repetidas = df_flat[df_flat['id'].isin(frecuencia_id[frecuencia_id > 1].index)]
+# Filtrar las filas donde el valor de 'id' aparezca más de x veces
+filas_repetidas = df_flat[df_flat['id'].isin(frecuencia_id[frecuencia_id > 3].index)]
 print(filas_repetidas)
 
-# 743 cosas hay repetidas
+# 743 cosas hay repetidas -> misma cosa, distinto padre, aunque las que mas se repiten son 4 veces
 
 
 
